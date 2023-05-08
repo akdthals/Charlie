9b44f6836d9b4c3550ec8f039ec94269a9746296
akdthals <mayaleana69@gmail.com>
2023-05-08 18:03:11 +0900

    0508

---
+++ apache-tomcat-9.0.73/webapps/ROOT/jsp/0508_MVC_모델링_프로젝트/쇼핑몰고객관리/src/kurly/UserDAO.java
@@ -0,0 +1,135 @@
+package kurly;
+import java.sql.*;
+import kurly.UserDTO;
+
+public class UserDAO {
+    private Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+
+    public UserDAO(){
+
+        try{
+                // 변수설정
+                String URL = "jdbc:mysql://localhost:3306/user";
+                String ID = "root";
+                String PW = "1234";
+                // 1. 데이터베이스 드라이버(JDBC)
+                Class.forName("com.mysql.jdbc.Driver");
+                System.out.println("JDBC Driver loading Success");
+
+                // 2. 데이터베이스 컨넥션 인증 & 인가(URL, ID, PW
+                conn = DriverManager.getConnection(URL, ID, PW);
+                System.out.println("Database Connection Success");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        } 
+    }
+
+    public int signup(UserDTO userDTO){
+        String SQL = "INSERT INTO kurly_table(user_id, user_pw, user_name, user_email, user_hp, user_addr, user_gender, user_birth) VALUES(?,?,?,?,?,?,?,?)";
+        try{
+            ps = conn.prepareStatement(SQL);
+            ps.setString(1, userDTO.getUser_id());
+            ps.setString(2, userDTO.getUser_pw());
+            ps.setString(3, userDTO.getUser_name());
+            ps.setString(4, userDTO.getUser_email());
+            ps.setString(5, userDTO.getUser_hp());
+            ps.setString(6, userDTO.getUser_addr());
+            ps.setString(7, userDTO.getUser_gender());
+            ps.setString(8, userDTO.getUser_birth());
+            return ps.executeUpdate();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return -1;
+    }
+    
+    public int signin(String user_id, String user_pw){
+        String SQL = "SELECT user_pw FROM kurly_table WHERE user_id=? ";
+        try{
+            ps = conn.prepareStatement(SQL);
+            ps.setString(1, user_id);
+
+            rs = ps.executeQuery();
+            if(rs.next()){
+                if(rs.getString(1).equals(user_pw)){
+                    return 1; 
+                }
+                else{
+                    return 0; 
+                }
+            }
+            else{
+                return -1; 
+            }
+        }
+        catch(Exception e ){
+            e.printStackTrace();
+        }
+        return -2; 
+    }
+    public int update(UserDTO userDTO){
+        String SQL = "UPDATE kurly_table SET user_pw=?, user_name=?, user_email=?, user_hp=?, user_addr=?, user_gender=?, user_birth=? WHERE user_id=?";
+        try{
+            ps = conn.prepareStatement(SQL);
+            ps.setString(1, userDTO.getUser_pw());
+            ps.setString(2, userDTO.getUser_name());
+            ps.setString(3, userDTO.getUser_email());
+            ps.setString(4, userDTO.getUser_hp());
+            ps.setString(5, userDTO.getUser_addr());
+            ps.setString(6, userDTO.getUser_gender());
+            ps.setString(7, userDTO.getUser_birth());
+            ps.setString(8, userDTO.getUser_id());
+            return ps.executeUpdate();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return -1;
+    }
+    public int delete(String user_id, String user_pw){
+        String SQL = "DELETE FROM kurly_table WHERE user_id=? AND user_pw=?";
+        try{
+            ps = conn.prepareStatement(SQL);
+            ps.setString(1, user_id);
+            ps.setString(2, user_pw);
+            
+            return ps.executeUpdate();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    // 개인정보 수정하기 위해서 업데이트 폼에 데이터 내보내기
+    public UserDTO getJoin(String user_id){
+        UserDTO userDTO = new UserDTO(); 
+
+        String SQL = "SELECT * FROM kurly_table WHERE user_id=? ";
+        try{
+            ps = conn.prepareStatement(SQL);
+            ps.setString(1, user_id);
+
+            rs = ps.executeQuery();
+            if(rs.next()){ // 데이터베이스 자료를 가져와서 DTO 세터에 저장
+                userDTO.setUser_id(user_id);
+                userDTO.setUser_pw(rs.getString("user_pw"));
+                userDTO.setUser_name(rs.getString("user_name"));
+                userDTO.setUser_email(rs.getString("user_email"));
+                userDTO.setUser_hp(rs.getString("user_hp"));
+                userDTO.setUser_addr(rs.getString("user_addr"));
+                userDTO.setUser_gender(rs.getString("user_gender"));
+                userDTO.setUser_birth(rs.getString("user_birth"));
+            }
+        }
+        catch(Exception e ){
+            e.printStackTrace();
+        }
+        return userDTO; 
+    }
+}
---
+++ apache-tomcat-9.0.73/webapps/ROOT/jsp/0508_MVC_모델링_프로젝트_혼자하기/쇼핑몰고객관리/kurly_user_signin_action.jsp
@@ -0,0 +1,82 @@
+<%@
+    page
+    language="java"
+    contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"
+%>
+
+<%@ page import = "kurly.UserDAO" %>
+
+<%  request.setCharacterEncoding("UTF-8");  %>
+
+<jsp:useBean id="userDTO" class="kurly.UserDTO" />
+<jsp:setProperty name="userDTO" property="user_id" />
+<jsp:setProperty name="userDTO" property="user_pw" />
+
+<%
+    if(
+           userDTO.getUser_id()==null
+        || userDTO.getUser_pw()==null
+    ){
+%>
+        <script>
+            alert("빈값은 허용하지 않습니다. 확인하고 다시 시도해주세요");
+            history.go(-1);
+        </script>
+
+
+<%   
+    }
+    else{
+        UserDAO userDAO = new UserDAO();   
+        int result = userDAO.signin(userDTO.getUser_id(), userDTO.getUser_pw());
+
+         if(result==1) { 
+            session.setAttribute("user_id", userDTO.getUser_id());    
+            
+        %>
+        <script>
+            alert( "로그인 되었습니다.");
+            location.href="./kurly_user_main.jsp";
+        </script>
+        <% 
+    }     
+    else if(result==0){
+    %>
+        <script>
+            alert( "로그인 실패 되었습니다. \n비밀번호 다시 확인");
+            history.go(-1);
+        </script>
+
+
+        <%
+        }
+           
+        else if(result==-1){ %>
+    
+        <script>
+            alert(<%= result %>)
+            alert( "로그인 실패 되었습니다. \아이디를 다시 확인");
+            window.location.href="kurly_user_signup.jsp";
+        </script>
+
+
+        <%
+        }
+    
+        
+       
+        else if(result==-2){ %>
+   
+        <script>
+            alert(<%= result %>)
+            alert( "로그인 실패 되었습니다. \다시 확인");
+            history.go(-1);
+        </script>
+
+
+        <%
+        
+    }
+}
+        %>
